455aa1e9213387815aead815458bc5908e6dff9d
David Thompson <dthompson@member.fsf.org>
2013-01-16 07:59:07 -0500

    Add wait procedure that yields from a coroutine and schedules the continuation in the current agenda.

--- module/gshmup/core.scm
+++ module/gshmup/core.scm
@@ -1,8 +1,18 @@
 (define-module (gshmup core)
   #:use-module (gshmup keycodes)
-  #:export (bind-key))
+  #:use-module (gshmup coroutine)
+  #:use-module (gshmup agenda)
+  #:export (bind-key
+            wait))
+
+(define %bind-key #f)
+(define game-schedule #f)
 
 (load-extension "./gshmup.so" "gshmup_init")
 
 (define (bind-key gshmup-key key)
   (%bind-key (gshmup-keycode gshmup-key) (keycode key)))
+
+(define (wait delay)
+  (abort-to-prompt 'coroutine-prompt
+                   (lambda (resume) (game-schedule delay resume))))
