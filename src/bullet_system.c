04f95f03273a7d1dfae2ad48c5d8496cc123f139
David Thompson <dthompson@member.fsf.org>
2013-01-16 21:21:49 -0500

    Convert GshmupBullet to an entity and refactor GshmupBulletSystem to use a GshmupEntityPool.

--- src/bullet_system.c
+++ src/bullet_system.c
@@ -1,55 +1,5 @@
 #include "bullet_system.h"
 
-static GshmupBullet *
-new_bullet (GshmupBulletSystem *system)
-{
-    GshmupBullet *bullet;
-
-    if (system->free_bullets) {
-        /* Remove bullet from front of free bullets list. */
-        bullet = system->free_bullets;
-        system->free_bullets = bullet->next;
-        system->pool_size--;
-    } else {
-        bullet = (GshmupBullet *) malloc (sizeof (GshmupBullet));
-    }
-
-    /* Insert bullet at front of active bullets list. */
-    bullet->next = system->active_bullets;
-    system->active_bullets = bullet;
-    system->bullet_count++;
-
-    return bullet;
-}
-
-static void
-free_bullet (GshmupBulletSystem *system, GshmupBullet *bullet)
-{
-    if (system->pool_size >= system->max_pool_size) {
-        /* Free the memory if there's no room in the pool. */
-        free (bullet);
-    } else {
-        system->pool_size++;
-        bullet->next = system->free_bullets;
-        system->free_bullets = bullet;
-    }
-
-    system->bullet_count--;
-}
-
-static void
-free_bullets (GshmupBullet *bullets)
-{
-    GshmupBullet *bullet = bullets;
-
-    while (bullet) {
-        GshmupBullet *next = bullet->next;
-
-        free (bullet);
-        bullet = next;
-    }
-}
-
 static void
 init_bullet_movement (GshmupBullet *bullet, float speed, float direction,
                       float acceleration, float angular_velocity)
@@ -62,102 +12,62 @@ init_bullet_movement (GshmupBullet *bullet, float speed, float direction,
                         gshmup_deg_to_rad (angular_velocity));
 }
 
-static void
-draw_bullet (GshmupBullet *bullet)
+static bool
+bullet_out_of_bounds (GshmupBullet *bullet)
 {
-    bullet->sprite.position = bullet->pos;
+    return !gshmup_rect_collide_point (bullet->parent->bounds, bullet->position);
+}
+
+void
+gshmup_draw_bullet (GshmupBullet *bullet)
+{
+    bullet->sprite.position = bullet->position;
     gshmup_draw_sprite (&bullet->sprite);
 }
 
-static void
-update_bullet (GshmupBullet *bullet)
+void
+gshmup_update_bullet (GshmupBullet *bullet)
 {
     al_transform_coordinates (&bullet->angular_velocity,
                               &bullet->vel.x, &bullet->vel.y);
     al_transform_coordinates (&bullet->angular_velocity,
                               &bullet->acc.x, &bullet->acc.y);
-    bullet->pos = gshmup_vector2_add (bullet->pos, bullet->vel);
+    bullet->position = gshmup_vector2_add (bullet->position, bullet->vel);
     bullet->vel = gshmup_vector2_add (bullet->vel, bullet->acc);
     bullet->life_count++;
-}
-
-static bool
-bullet_out_of_bounds (GshmupBulletSystem *system, GshmupBullet *bullet)
-{
-    return !gshmup_rect_collide_point (system->bounds, bullet->pos);
+    bullet->kill = bullet->kill || bullet_out_of_bounds (bullet);
 }
 
 GshmupBulletSystem *
-gshmup_create_bullet_system (int max_pool_size)
+gshmup_create_bullet_system (int max_bullets)
 {
     GshmupBulletSystem *system;
 
     system = (GshmupBulletSystem *) malloc (sizeof (GshmupBulletSystem));
-    system->max_pool_size = max_pool_size;
-    system->pool_size = 0;
-    system->bullet_count = 0;
-    system->active_bullets = NULL;
-    system->free_bullets = NULL;
-
+    system->bullets = gshmup_create_entity_pool (max_bullets);
+    system->bounds = gshmup_create_rect (0, 0, 0, 0);
     return system;
 }
 
 void
 gshmup_destroy_bullet_system (GshmupBulletSystem *system)
 {
-    free_bullets (system->active_bullets);
-    free_bullets (system->free_bullets);
+    gshmup_destroy_entity_pool (system->bullets);
     free (system);
 }
 
 void
 gshmup_draw_bullet_system (GshmupBulletSystem *system)
 {
-    GshmupBullet *bullet = system->active_bullets;
-
     al_hold_bitmap_drawing (true);
-
-    while (bullet) {
-        draw_bullet (bullet);
-        bullet = bullet->next;
-    }
-
+    gshmup_draw_entity_pool (system->bullets);
     al_hold_bitmap_drawing (false);
 }
 
 void
 gshmup_update_bullet_system (GshmupBulletSystem *system)
 {
-    GshmupBullet *bullet = system->active_bullets;
-    GshmupBullet *prev = NULL;
-
-    while (bullet) {
-        GshmupBullet *next = bullet->next;
-
-        update_bullet (bullet);
-
-        if (bullet->kill || bullet_out_of_bounds (system, bullet)) {
-            /* Remove dead bullet from active list. */
-            if (prev) {
-                prev->next = next;
-            } else {
-                system->active_bullets = next;
-            }
-
-            /* Place dead bullet in free list. */
-            free_bullet (system, bullet);
-
-            /* Set bullet to prev so that iterator is correct. */
-        } else {
-            /*
-             * Prev only changes when we aren't pruning a bullet from
-             * the active list.
-             */
-            prev = bullet;
-        }
-
-        bullet = next;
-    }
+    gshmup_update_entity_pool (system->bullets);
 }
 
 void
@@ -165,12 +75,33 @@ gshmup_emit_bullet (GshmupBulletSystem *system, ALLEGRO_BITMAP *image,
                     GshmupVector2 position, float speed, float direction,
                     float acceleration, float angular_velocity, float life)
 {
-    GshmupBullet *bullet = new_bullet (system);
+    GshmupEntity *entity = gshmup_entity_pool_new (system->bullets);
 
-    bullet->pos = position;
-    bullet->life = life;
-    bullet->life_count = 0;
-    bullet->kill = false;
-    gshmup_init_sprite (&bullet->sprite, image);
-    init_bullet_movement (bullet, speed, direction, acceleration, angular_velocity);
+    entity->type = GSHMUP_ENTITY_BULLET;
+    entity->bullet.parent = system;
+    entity->bullet.position = position;
+    entity->bullet.life = life;
+    entity->bullet.life_count = 0;
+    entity->bullet.kill = false;
+    gshmup_init_sprite (&entity->bullet.sprite, image);
+    init_bullet_movement (GSHMUP_BULLET (entity), speed, direction,
+                          acceleration, angular_velocity);
+}
+
+int
+gshmup_get_bullet_system_size (GshmupBulletSystem *system)
+{
+    return system->bullets->size;
+}
+
+int
+gshmup_get_bullet_system_free_size (GshmupBulletSystem *system)
+{
+    return system->bullets->pool_size;
+}
+
+int
+gshmup_get_bullet_system_max_free_size (GshmupBulletSystem *system)
+{
+    return system->bullets->max_pool_size;
 }
--- src/bullet_system.h
+++ src/bullet_system.h
@@ -1,40 +1,43 @@
+#include "entity.h"
+
 #ifndef GSHMUP_BULLET_SYSTEM_H
 #define GSHMUP_BULLET_SYSTEM_H
 
 #include "common.h"
 #include "rect.h"
 #include "sprite.h"
+#include "entity_pool.h"
 
-typedef struct GshmupBullet GshmupBullet;
-struct GshmupBullet {
+typedef struct {
+    GshmupEntityPool *bullets;
+    GshmupRect bounds;
+} GshmupBulletSystem;
+
+typedef struct {
+    _GSHMUP_ENTITY_HEADER
     int life;         /* Maximum lifetime. 0 is unlimited. */
     int life_count;   /* Total elapsed lifetime. */
-    bool kill;        /* Remove the bullet on next update? */
     bool directional; /* Rotate sprite in bullet direction? */
-    GshmupVector2 pos;
     GshmupVector2 vel;
     GshmupVector2 acc;
     GshmupRect hitbox;
     ALLEGRO_TRANSFORM angular_velocity; /* Change in direction. */
     GshmupSprite sprite;
-    GshmupBullet *next;
-};
+    GshmupBulletSystem *parent;
+} GshmupBullet;
 
-typedef struct {
-    int max_pool_size;
-    int pool_size;
-    int bullet_count;
-    GshmupBullet *active_bullets;
-    GshmupBullet *free_bullets;
-    GshmupRect bounds;
-} GshmupBulletSystem;
+void gshmup_draw_bullet (GshmupBullet *bullet);
+void gshmup_update_bullet (GshmupBullet *bullet);
 
-GshmupBulletSystem *gshmup_create_bullet_system (int max_pool_size);
+GshmupBulletSystem *gshmup_create_bullet_system (int max_bullets);
 void gshmup_destroy_bullet_system (GshmupBulletSystem *system);
 void gshmup_draw_bullet_system (GshmupBulletSystem *system);
 void gshmup_update_bullet_system (GshmupBulletSystem *system);
 void gshmup_emit_bullet (GshmupBulletSystem *system, ALLEGRO_BITMAP *image,
                          GshmupVector2 position, float speed, float direction,
                          float acceleration, float angular_velocity, float life);
+int gshmup_get_bullet_system_size (GshmupBulletSystem *system);
+int gshmup_get_bullet_system_free_size (GshmupBulletSystem *system);
+int gshmup_get_bullet_system_max_free_size (GshmupBulletSystem *system);
 
 #endif
