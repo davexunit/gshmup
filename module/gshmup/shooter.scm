1e3ed4849e2145531624a02eca7b3e84ce6f3ba1
David Thompson <dthompson@member.fsf.org>
2013-01-21 13:37:58 -0500

    Update demo.

--- module/gshmup/shooter.scm
+++ module/gshmup/shooter.scm
@@ -16,8 +16,8 @@
                       (make-bullet-type 3 (make-rect -4 -4 8 8) 'alpha #t))
 (register-bullet-type 'bright
                       (make-bullet-type 4 (make-rect -8 -8 16 16) 'add #f))
-(register-bullet-type 'small-green
-                      (make-bullet-type 5 (make-rect -2 -2 4 4) 'alpha #f))
+(register-bullet-type 'fire
+                      (make-bullet-type 5 (make-rect -2 -2 4 4) 'alpha #t))
 
 (add-hook! player-shoot-hook (lambda () (test-shot)))
 
@@ -35,20 +35,20 @@
     (wait 5)
     (test-shot)))
 
-;;(add-hook! shooter-init-hook (lambda () (spawn-test-enemy)))
+(add-hook! shooter-init-hook (lambda () (spawn-test-enemy)))
 
 (define (spawn-test-enemy)
-  (spawn-enemy (make-vector2 240 240) (lambda () (test-pattern))))
+  (spawn-enemy (make-vector2 120 64) (lambda () (test-pattern))))
 
 (define-coroutine (test-pattern)
   (let fire ((a 0)
              (n 16))
     (repeat n (lambda (i)
-                (emit-bullet (entity-position) 3 (+ a (* 360 (/ i n))) 'medium-blue)))
-    (repeat n (lambda (i)
-                (emit-bullet (entity-position) 3 (- (* 360 (/ i n)) a) 'small-green)))
-    (wait 7)
-    (fire (+ a 2) n)))
+                (emit-bullet (entity-position) 3 (+ a (* 360 (/ i n))) 'fire)))
+    ;; (repeat n (lambda (i)
+    ;;             (emit-bullet (entity-position) 3 (- (* 360 (/ i n)) a) 'fire)))
+    (wait 12)
+    (fire (+ a 6) n)))
 
 (define-coroutine (test-move)
   (repeat 120 (lambda (i)
