00b34b620a60894bfc8d7500d066d333e2c76ebe
David Thompson <dthompson@member.fsf.org>
2013-01-21 12:03:26 -0500

    Add bullet module.

---
+++ module/gshmup/bullet.scm
@@ -0,0 +1,26 @@
+(define-module (gshmup bullet)
+  #:use-module (gshmup core)
+  #:export (get-bullet-type
+            register-bullet-type
+            emit-bullet
+            set-bullet-type))
+
+(define bullet-types (make-hash-table))
+
+(define (register-bullet-type key type)
+  "Associates a key with a procedure for setting particular bullet properties."
+  (hash-set! bullet-types key type))
+
+(define (get-bullet-type key)
+  "Applies the bullet properties associated with the given key."
+  (hash-ref bullet-types key))
+
+;;(define* (emit-bullet system pos speed direction type . keyword-args)
+(define (emit-bullet position speed direction type)
+  "Emits a non-scripted bullet."
+  (let ((type (get-bullet-type type)))
+    (%emit-bullet position speed direction type)))
+
+(define (set-bullet-type bullet type)
+  (let ((type (get-bullet-type type)))
+    (%set-bullet-type bullet type)))
