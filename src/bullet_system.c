eb9d6196bc455f842cb916a24aee8ea67126c48d
David Thompson <dthompson@member.fsf.org>
2013-01-21 12:03:50 -0500

    Add GshmupBulletType for describing common properties of bullets.

--- src/bullet_system.c
+++ src/bullet_system.c
@@ -1,5 +1,83 @@
 #include "bullet_system.h"
 
+static scm_t_bits bullet_type_tag;
+SCM_SYMBOL (sym_blend_alpha, "alpha");
+SCM_SYMBOL (sym_blend_add, "add");
+
+static int
+scm_to_blend_mode (SCM blend_mode)
+{
+    int mode;
+
+    if (scm_is_true (scm_eq_p (blend_mode, sym_blend_alpha))) {
+        mode = GSHMUP_BULLET_BLEND_ALPHA;
+    } else if (scm_is_true (scm_eq_p (blend_mode, sym_blend_add))) {
+        mode = GSHMUP_BULLET_BLEND_ADD;
+    } else {
+        scm_error_scm (scm_from_latin1_symbol ("blend-mode-error"),
+                       SCM_BOOL_F,
+                       scm_from_locale_string ("Unrecognized blend mode"),
+                       SCM_EOL, SCM_BOOL_F);
+    }
+
+    return mode;
+}
+
+static void
+set_blend_mode (int blend_mode)
+{
+    switch (blend_mode) {
+    case GSHMUP_BULLET_BLEND_ALPHA:
+        al_set_blender(ALLEGRO_ADD, ALLEGRO_ONE, ALLEGRO_INVERSE_ALPHA);
+        break;
+    case GSHMUP_BULLET_BLEND_ADD:
+        al_set_blender(ALLEGRO_ADD, ALLEGRO_ALPHA, ALLEGRO_ONE);
+        break;
+    }
+}
+
+GshmupBulletType *
+check_bullet_type (SCM bullet_type)
+{
+    scm_assert_smob_type (bullet_type_tag, bullet_type);
+
+    return (GshmupBulletType *) SCM_SMOB_DATA (bullet_type);
+}
+
+SCM_DEFINE (make_bullet_type, "make-bullet-type", 4, 0, 0,
+            (SCM tile, SCM hitbox, SCM blend_mode, SCM directional),
+            "Make a new bullet type instance.")
+{
+    GshmupBulletType *bullet_type;
+
+    bullet_type = (GshmupBulletType *) scm_gc_malloc (sizeof (GshmupBulletType),
+                                                      "bullet_type");
+    bullet_type->tile = scm_to_int (tile);
+    bullet_type->hitbox = gshmup_scm_to_rect (hitbox);
+    bullet_type->blend_mode = scm_to_blend_mode (blend_mode);
+    bullet_type->directional = scm_to_bool (directional);
+
+    SCM_RETURN_NEWSMOB (bullet_type_tag, bullet_type);
+}
+
+static size_t
+free_bullet_type (SCM bullet_type_smob)
+{
+    GshmupBulletType *bullet_type = (GshmupBulletType *) SCM_SMOB_DATA (bullet_type_smob);
+
+    scm_gc_free (bullet_type, sizeof (GshmupBulletType), "bullet_type");
+
+    return 0;
+}
+
+static int
+print_bullet_type (SCM bullet_type_smob, SCM port, scm_print_state *pstate)
+{
+   scm_puts ("#<bullet-type>", port);
+
+    return 1;
+}
+
 static void
 init_bullet_movement (GshmupBullet *bullet, float speed, float direction,
                       float acceleration, float angular_velocity)
@@ -36,15 +114,20 @@ gshmup_update_bullet (GshmupBullet *bullet)
     bullet->vel = gshmup_vector2_add (bullet->vel, bullet->acc);
     bullet->life_count++;
     bullet->kill = bullet->kill || bullet_out_of_bounds (bullet);
+
+    if (bullet->directional) {
+        bullet->sprite.rotation = gshmup_vector2_angle (bullet->vel);
+    }
 }
 
 GshmupBulletSystem *
-gshmup_create_bullet_system (int max_bullets)
+gshmup_create_bullet_system (int max_bullets, GshmupSpriteSheet *sprite_sheet)
 {
     GshmupBulletSystem *system;
 
     system = (GshmupBulletSystem *) scm_gc_malloc (sizeof (GshmupBulletSystem),
                                                    "bullet system");
+    system->sprite_sheet = sprite_sheet;
     system->bullets = gshmup_create_entity_pool (max_bullets);
     system->bounds = gshmup_create_rect (0, 0, 0, 0);
 
@@ -73,9 +156,22 @@ gshmup_update_bullet_system (GshmupBulletSystem *system)
 }
 
 void
-gshmup_emit_bullet (GshmupBulletSystem *system, ALLEGRO_BITMAP *image,
-                    GshmupVector2 position, float speed, float direction,
-                    float acceleration, float angular_velocity, float life)
+gshmup_set_bullet_type (GshmupEntity *entity, GshmupBulletType *type)
+{
+    GshmupBulletSystem *parent = entity->bullet.parent;
+    ALLEGRO_BITMAP *image = gshmup_get_sprite_sheet_tile (parent->sprite_sheet,
+                                                         type->tile);
+
+    gshmup_init_sprite (&entity->bullet.sprite, image);
+    entity->bullet.directional = type->directional;
+    entity->bullet.hitbox = type->hitbox;
+    entity->bullet.blend_mode = type->blend_mode;
+}
+
+void
+gshmup_emit_bullet (GshmupBulletSystem *system, GshmupVector2 position,
+                    float speed, float direction, float acceleration,
+                    float angular_velocity, float life, GshmupBulletType *type)
 {
     GshmupEntity *entity = gshmup_entity_pool_new (system->bullets);
 
@@ -85,7 +181,7 @@ gshmup_emit_bullet (GshmupBulletSystem *system, ALLEGRO_BITMAP *image,
     entity->bullet.life = life;
     entity->bullet.life_count = 0;
     entity->bullet.kill = false;
-    gshmup_init_sprite (&entity->bullet.sprite, image);
+    gshmup_set_bullet_type (entity, type);
     init_bullet_movement (GSHMUP_BULLET (entity), speed, direction,
                           acceleration, angular_velocity);
 }
@@ -106,4 +202,17 @@ int
 gshmup_get_bullet_system_max_free_size (GshmupBulletSystem *system)
 {
     return system->bullets->max_pool_size;
+}
+
+void
+gshmup_bullet_system_init_scm (void)
+{
+    bullet_type_tag = scm_make_smob_type ("<bullet-type>", sizeof (GshmupBulletType));
+    scm_set_smob_mark (bullet_type_tag, 0);
+    scm_set_smob_free (bullet_type_tag, free_bullet_type);
+    scm_set_smob_print (bullet_type_tag, print_bullet_type);
+
+#include "bullet_system.x"
+
+    scm_c_export (s_make_bullet_type, NULL);
 }
--- src/bullet_system.h
+++ src/bullet_system.h
@@ -8,7 +8,22 @@
 #include "sprite.h"
 #include "entity_pool.h"
 
+/* Blending modes */
+enum {
+    GSHMUP_BULLET_BLEND_ALPHA,
+    GSHMUP_BULLET_BLEND_ADD,
+};
+
+/* Describes common properties for a whole class of bullets. */
 typedef struct {
+    int tile;
+    int blend_mode;
+    bool directional;
+    GshmupRect hitbox;
+} GshmupBulletType;
+
+typedef struct {
+    GshmupSpriteSheet *sprite_sheet;
     GshmupEntityPool *bullets;
     GshmupRect bounds;
 } GshmupBulletSystem;
@@ -23,21 +38,29 @@ typedef struct {
     GshmupRect hitbox;
     ALLEGRO_TRANSFORM angular_velocity; /* Change in direction. */
     GshmupSprite sprite;
+    ALLEGRO_COLOR color;
+    int blend_mode;
     GshmupBulletSystem *parent;
 } GshmupBullet;
+
+GshmupBulletType *check_bullet_type (SCM bullet_type);
 
 void gshmup_draw_bullet (GshmupBullet *bullet);
 void gshmup_update_bullet (GshmupBullet *bullet);
 
-GshmupBulletSystem *gshmup_create_bullet_system (int max_bullets);
+GshmupBulletSystem *gshmup_create_bullet_system (int max_bullets,
+                                                 GshmupSpriteSheet *sprite_sheet);
 void gshmup_destroy_bullet_system (GshmupBulletSystem *system);
 void gshmup_draw_bullet_system (GshmupBulletSystem *system);
 void gshmup_update_bullet_system (GshmupBulletSystem *system);
-void gshmup_emit_bullet (GshmupBulletSystem *system, ALLEGRO_BITMAP *image,
-                         GshmupVector2 position, float speed, float direction,
-                         float acceleration, float angular_velocity, float life);
+void gshmup_set_bullet_type (GshmupEntity *entity, GshmupBulletType *type);
+void gshmup_emit_bullet (GshmupBulletSystem *system, GshmupVector2 position,
+                         float speed, float direction, float acceleration,
+                         float angular_velocity, float life,
+                         GshmupBulletType *type);
 int gshmup_get_bullet_system_size (GshmupBulletSystem *system);
 int gshmup_get_bullet_system_free_size (GshmupBulletSystem *system);
 int gshmup_get_bullet_system_max_free_size (GshmupBulletSystem *system);
+void gshmup_bullet_system_init_scm (void);
 
 #endif
