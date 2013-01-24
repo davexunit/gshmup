0eaf1885d5d1694004035fca2f2c9f99c10720f0
David Thompson <dthompson@member.fsf.org>
2013-01-23 20:34:36 -0500

    Update demo to use new bullet type API.

--- module/gshmup/shooter.scm
+++ module/gshmup/shooter.scm
@@ -8,17 +8,18 @@
 
 ;; Bullet types
 (register-bullet-type 'medium-blue
-                      (make-bullet-type 0 (make-rect -4 -4 8 8) 'alpha #f))
+                      (make-bullet-type 0 (make-rect -4 -4 8 8) 'alpha #f kill-player))
 (register-bullet-type 'small-diamond
-                      (make-bullet-type 1 (make-rect -2 -2 4 4) 'alpha #t))
+                      (make-bullet-type 1 (make-rect -2 -2 4 4) 'alpha #t kill-player))
 (register-bullet-type 'large-orange
-                      (make-bullet-type 2 (make-rect -5 -5 10 10) 'alpha #f))
+                      (make-bullet-type 2 (make-rect -5 -5 10 10) 'alpha #f kill-player))
 (register-bullet-type 'blue
-                      (make-bullet-type 3 (make-rect -3 -3 6 6) 'alpha #t))
+                      (make-bullet-type 3 (make-rect -3 -3 6 6) 'alpha #t
+                                        (lambda () (damage-enemy 1))))
 (register-bullet-type 'bright
-                      (make-bullet-type 4 (make-rect -8 -8 16 16) 'add #f))
+                      (make-bullet-type 4 (make-rect -8 -8 16 16) 'add #f kill-player))
 (register-bullet-type 'fire
-                      (make-bullet-type 5 (make-rect -2 -2 4 4) 'alpha #t))
+                      (make-bullet-type 5 (make-rect -2 -2 4 4) 'alpha #t kill-player))
 
 (add-hook! player-shoot-hook (lambda () (test-shot)))
 
