bcc0cbe0152638f9e972c0136ce0ade1337f0ce8
David Thompson <dthompson@member.fsf.org>
2013-02-09 12:30:01 -0500

    Remove pretty much everything from entity.

--- src/entity.h
+++ src/entity.h
@@ -4,47 +4,12 @@
 #include "common.h"
 
 /*
- * Entities represent the objects in the game such as the player,
- * enemy, and bullets.
- *
- * GshmupEntity is a union of all possible entity types, providing a
- * simple implementation of polymorphism.
+ * Common properties for game objects.
  */
+typedef struct {
+    /* Placeholder. Might use this. Might not. */
+} GshmupEntity;
 
-typedef enum {
-    GSHMUP_ENTITY_ANY,
-} GshmupEntityType;
-
-typedef union GshmupEntity GshmupEntity;
-
-#define _GSHMUP_ENTITY_HEADER \
-    GshmupEntityType type; \
-    SCM agenda; \
-    bool kill; \
-    GshmupVector2 position; \
-    GshmupEntity *next;
-
-#include "enemy.h"
-
-union GshmupEntity {
-    GshmupEntityType type;
-    /*
-     * The base struct allows changing all common member variables in
-     * _GSHMUP_ENTITY_TYPE without caring about the entity type.
-     */
-    struct {
-        _GSHMUP_ENTITY_HEADER
-    } base;
-};
-
-GshmupEntity *gshmup_create_entity (void);
-void gshmup_init_entity (GshmupEntity *entity);
-void gshmup_destroy_entity (GshmupEntity *entity);
-void gshmup_draw_entity (GshmupEntity *entity);
-void gshmup_update_entity (GshmupEntity *entity);
-void gshmup_entity_schedule (GshmupEntity *entity, int dt, SCM thunk);
-void gshmup_kill_entity (GshmupEntity *entity);
-void gshmup_entity_clear_agenda (GshmupEntity *entity);
 void gshmup_entity_init_scm (void);
 
 #endif
