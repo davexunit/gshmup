5399eb6dc0bfa0c800414c47cad3b54a2ec098ea
David Thompson <dthompson@member.fsf.org>
2013-02-09 15:39:50 -0500

    Add functions to create, draw, and update entities.

--- src/entity.c
+++ src/entity.c
@@ -1,6 +1,39 @@
 #include "entity.h"
+#include "string.h"
 
 /* static GshmupEntity *current_entity = NULL; */
+
+GshmupEntity
+gshmup_create_entity (const char *name)
+{
+    GshmupEntity entity;
+
+    strncpy (entity.name, name, GSHMUP_MAX_NAME_LENGTH - 1);
+    entity.name[GSHMUP_MAX_NAME_LENGTH] = '\0';
+    entity.position = gshmup_create_vector2 (0, 0);
+    entity.hitbox = gshmup_create_rect (0, 0, 0, 0);
+    entity.sprite_sheet = NULL;
+    entity.agenda = gshmup_create_agenda ();
+
+    return entity;
+}
+
+void
+gshmup_draw_entity (GshmupEntity *entity)
+{
+    entity->sprite.position = entity->position;
+    gshmup_draw_sprite (&entity->sprite);
+}
+
+void
+gshmup_update_entity (GshmupEntity *entity)
+{
+    gshmup_update_agenda (entity->agenda);
+
+    if (entity->sprite.type == GSHMUP_SPRITE_ANIMATED) {
+        gshmup_update_animation (entity->sprite.anim);
+    }
+}
 
 void
 gshmup_entity_init_scm (void)
--- src/entity.h
+++ src/entity.h
@@ -5,11 +5,13 @@
 #include "rect.h"
 #include "sprite.h"
 
+#define GSHMUP_MAX_NAME_LENGTH 128
+
 /*
  * Common properties for game objects.
  */
 typedef struct {
-    const char *name;
+    char name[GSHMUP_MAX_NAME_LENGTH];
     GshmupVector2 position;
     GshmupRect hitbox;
     GshmupSpriteSheet *sprite_sheet;
@@ -17,6 +19,9 @@ typedef struct {
     SCM agenda;
 } GshmupEntity;
 
+GshmupEntity gshmup_create_entity (const char *name);
+void gshmup_draw_entity (GshmupEntity *entity);
+void gshmup_update_entity (GshmupEntity *entity);
 void gshmup_entity_init_scm (void);
 
 #endif
