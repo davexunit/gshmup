7b5b9da8c3484af4b05ae6d5a1232ed7af5f4c90
David Thompson <dthompson@member.fsf.org>
2013-02-09 19:13:57 -0500

    Initialize splash screen Guile bindings.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
 #include "common.h"
 #include "rect.h"
 #include "game.h"
-#include "shooter.h"
 #include "player.h"
 #include "enemy.h"
 #include "bullet_system.h"
 #include "entity.h"
+#include "shooter.h"
+#include "splash_screen.h"
 
 SCM_DEFINE (gshmup_s_start, "gshmup-start", 0, 0, 0,
             (void),
@@ -13,7 +14,7 @@ SCM_DEFINE (gshmup_s_start, "gshmup-start", 0, 0, 0,
 {
     printf ("Starting gshmup...\n");
     gshmup_init_game ();
-    gshmup_set_current_scene (gshmup_create_shooter_scene ());
+    gshmup_set_current_scene (gshmup_create_splash_screen_scene ());
     gshmup_run_game ();
     printf ("Exiting...\n");
 
@@ -33,5 +34,6 @@ gshmup_init (void)
     gshmup_player_init_scm ();
     gshmup_enemy_init_scm ();
     gshmup_game_init_scm ();
+    gshmup_splash_screen_init_scm ();
     gshmup_shooter_init_scm ();
 }
