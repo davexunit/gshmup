91f96a389a615ea06b5db2967ecaccc30085230f
David Thompson <dthompson@member.fsf.org>
2013-02-09 19:14:05 -0500

    Update demo.

--- module/gshmup/init.scm
+++ module/gshmup/init.scm
@@ -2,6 +2,7 @@
   #:use-module (gshmup core)
   #:use-module (gshmup shooter))
 
+;; Debug
 (set! debug-mode #f)
 
 ;; Set preferred keybindings.
@@ -12,6 +13,10 @@
 (bind-key 'shoot 'z)
 (bind-key 'bomb 'x)
 (bind-key 'start 'enter)
+
+;; Splash screen settings
+(set! splash-screen-background "data/sprites/splash_background.png")
+(set! splash-screen-title "data/sprites/splash_title.png")
 
 ;; Player settings.
 (set! num-credits 5)
