7eeba6c4bb27ee954c53486add8733e90c18d171
David Thompson <dthompson@member.fsf.org>
2013-02-17 21:09:31 -0500

    Add angle-to-player helper procedure.

--- module/gshmup/helpers.scm
+++ module/gshmup/helpers.scm
@@ -1,8 +1,14 @@
 (define-module (gshmup helpers)
-  #:export (repeat))
+  #:use-module (gshmup core)
+  #:export (repeat
+            angle-to-player))
 
 (define (repeat times proc)
   "Do something a bunch of times."
   (do ((i 1 (1+ i)))
        ((> i times))
     (proc i)))
+
+(define (angle-to-player)
+  "Compute angle in degrees from the current entity to the player."
+  (vector2-angle (vector2-sub (player-position) (entity-position))))
