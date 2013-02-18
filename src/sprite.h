a0a58c375a4867170cdb0102ecd794ba82d5195c
David Thompson <dthompson@member.fsf.org>
2013-02-18 02:35:16 -0500

    Remove animation stuff from sprite module.

--- src/sprite.h
+++ src/sprite.h
@@ -11,24 +11,6 @@ enum {
     GSHMUP_SPRITE_ANIMATED,
 };
 
-/* Animation modes */
-enum {
-    GSHMUP_ANIM_ONCE,
-    GSHMUP_ANIM_LOOP,
-};
-
-/* Sprite animation */
-typedef struct {
-    GshmupSpriteSheet *sprite_sheet;
-    int duration;
-    int timer;
-    int num_frames;
-    int current_frame;
-    int *frames;
-    int anim_mode;
-    bool playing;
-} GshmupAnimation;
-
 typedef struct {
     int type;
     union {
@@ -41,14 +23,6 @@ typedef struct {
     GshmupVector2 anchor;
     float rotation;
 } GshmupSprite;
-
-GshmupAnimation *gshmup_create_animation (GshmupSpriteSheet *sprite_sheet, int duration,
-                                          int num_frames, int *frames, int anim_mode);
-void gshmup_destroy_animation (GshmupAnimation *anim);
-ALLEGRO_BITMAP *gshmup_get_animation_image (GshmupAnimation *anim);
-void gshmup_update_animation (GshmupAnimation *anim);
-void gshmup_play_animation (GshmupAnimation *anim);
-void gshmup_stop_animation (GshmupAnimation *anim);
 
 void gshmup_init_sprite (GshmupSprite *sprite, ALLEGRO_BITMAP *image);
 void gshmup_init_animated_sprite (GshmupSprite *sprite, GshmupAnimation *anim);
