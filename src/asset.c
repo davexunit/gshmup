31022617ea6a38fffa9c9e2e2861397e97cbdccc
David Thompson <dthompson@member.fsf.org>
2013-02-18 00:52:14 -0500

    Add sprite sheets as a type of asset.

--- src/asset.c
+++ src/asset.c
@@ -1,6 +1,7 @@
 #include "asset.h"
 
 GHashTable *images;
+GHashTable *sprite_sheets;
 
 static void
 destroy_image (gpointer data)
@@ -10,10 +11,21 @@ destroy_image (gpointer data)
     al_destroy_bitmap (image);
 }
 
+static void
+destroy_sprite_sheet (gpointer data)
+{
+    GshmupSpriteSheet *sprite_sheet = (GshmupSpriteSheet *) data;
+
+    gshmup_destroy_sprite_sheet (sprite_sheet);
+}
+
 void
 gshmup_init_assets (void)
 {
-    images = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, destroy_image);
+    images = g_hash_table_new_full (g_str_hash, g_str_equal,
+                                    g_free, destroy_image);
+    sprite_sheets = g_hash_table_new_full (g_str_hash, g_str_equal,
+                                           g_free, destroy_sprite_sheet);
 }
 
 void
@@ -29,7 +41,7 @@ gshmup_free_assets (void)
 }
 
 ALLEGRO_BITMAP *
-gshmup_load_image (const char *filename)
+gshmup_load_image_asset (const gchar *filename)
 {
     ALLEGRO_BITMAP *image = (ALLEGRO_BITMAP *) g_hash_table_lookup (images, filename);
 
@@ -39,4 +51,18 @@ gshmup_load_image (const char *filename)
     }
 
     return image;
+}
+
+GshmupSpriteSheet *
+gshmup_load_sprite_sheet_asset (const gchar *filename)
+{
+    GshmupSpriteSheet *sprite_sheet =
+        (GshmupSpriteSheet *) g_hash_table_lookup (sprite_sheets, filename);
+
+    if (!sprite_sheet) {
+        sprite_sheet = gshmup_load_sprite_sheet (filename);
+        g_hash_table_insert (sprite_sheets, g_strdup (filename), sprite_sheet);
+    }
+
+    return sprite_sheet;
 }
--- src/asset.h
+++ src/asset.h
@@ -2,10 +2,12 @@
 #define GSHMUP_ASSET_H
 
 #include "common.h"
+#include "sprite_sheet.h"
 
 void gshmup_init_assets (void);
 void gshmup_destroy_assets (void);
 void gshmup_free_assets (void);
-ALLEGRO_BITMAP *gshmup_load_image (const char *filename);
+ALLEGRO_BITMAP *gshmup_load_image_asset (const gchar *filename);
+GshmupSpriteSheet *gshmup_load_sprite_sheet_asset (const gchar *filename);
 
 #endif
