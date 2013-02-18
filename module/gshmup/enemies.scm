e6219d49db2af4840682f748e5666a4b61843787
David Thompson <dthompson@member.fsf.org>
2013-02-18 10:15:56 -0500

    Update demo.

--- module/gshmup/enemies.scm
+++ module/gshmup/enemies.scm
@@ -3,7 +3,10 @@
   #:export (enemy-test))
 
 (define enemy-test
-  (make-enemy-type "data/sprites/enemy_1.sheet" 60
+  (make-enemy-type "Testing"
+                   "data/sprites/enemy_1.sheet"
+                   "idle"
+                   60
                    (make-rect -16 -16 32 32)
                    (lambda () (test-on-death))))
 
